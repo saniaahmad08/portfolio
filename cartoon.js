064c6b5f6255f5100518ae85bbb611c57540c0d6
saniaahmad08 <saniaahmad218@gmail.com>
2026-03-14 03:23:45 -0400

    Update cartoon.js

--- cartoon.js
+++ cartoon.js
@@ -3,4 +3,8 @@ const ctx = myCanvas.getContext("2d");
 ctx.fillStyle = "black";
 ctx.font = "30px Arial";
 ctx.fillText("Sania's Cartoon",200,40);
+ctx.fillStyle = "LightCyan";
+ctx.fillRect(0,0,myCanvas.width,myCanvas.height);
+ctx.fillStyle = "Gold";
+ctx.arc(0,0,30,0,100);
 
