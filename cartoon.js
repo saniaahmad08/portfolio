c3309390d9be6a56cdc8112f512525ffac691c50
saniaahmad08 <saniaahmad218@gmail.com>
2026-03-14 02:58:13 -0400

    Update cartoon.js

--- cartoon.js
+++ cartoon.js
@@ -1,3 +1,9 @@
 const myCanvas = document.getElementById("myCanvas");
 const ctx = myCanvas.getContext("2d");
-ctx.fillRect(20, 20, 150, 100);
+ctx.beginPath();
+ctx.moveTo(20, 20);
+ctx.lineTo(20, 100);
+ctx.lineTo(70, 100);
+ctx.strokeStyle = "red";
+ctx.stroke();
+
