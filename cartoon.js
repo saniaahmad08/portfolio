9877969f91e119ffaf7c75218d4ea5715d345421
saniaahmad08 <saniaahmad218@gmail.com>
2026-03-14 03:06:27 -0400

    Update cartoon.js

--- cartoon.js
+++ cartoon.js
@@ -2,10 +2,4 @@ const myCanvas = document.getElementById("myCanvas");
 const ctx = myCanvas.getContext("2d");
 ctx.font = "4px Arial";
 ctx.fillText("Sania's Cartoon", 150,150);
-ctx.beginPath();
-ctx.moveTo(20, 20);
-ctx.lineTo(20, 100);
-ctx.lineTo(70, 100);
-ctx.strokeStyle = "red";
-ctx.stroke();
 
