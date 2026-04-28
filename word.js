e0b66715fb957c805effecb97e9de4abd36e4469
saniaahmad08 <saniaahmad218@gmail.com>
2026-04-28 12:05:16 -0400

    Update word.js

--- word.js
+++ word.js
@@ -2,10 +2,23 @@ function syncWord() {
  const menu = document.getElementById("wordSelect");
  const display = document.getElementById("wordDisplay");
  if (menu.selectedIndex > 0) {
- const selectedOption = menu.options[menu.selectedIndex];
- display.value = selectedOption.dataset.def;
+  const selectedOption = menu.options[menu.selectedIndex];
+  display.value = selectedOption.dataset.def;
  } 
  else {
- display.value = "";
+  display.value = "";
  }
 } 
+function speak(textToSay) {
+ const message = new SpeechSynthesisUtterance(textToSay);
+ message.pitch = 1.2;
+ message.rate = 1.0;
+ window.speechSynthesis.speak(message);
+} 
+function speakWord(){
+ const menu = document.getElementById("wordSelect");
+ if (menu.selectedIndex > 0) {
+  const selectedOption = menu.options[menu.selectedIndex];
+  speak(word);
+ }
+}
